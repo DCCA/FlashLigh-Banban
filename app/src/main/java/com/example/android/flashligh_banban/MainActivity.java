821a37725c8992ff48f319ba4420c773cae6fe70
Daniel Andrade <dcca12@gmail.com>
2016-12-22 13:57:17 -0200

    almost finishing. Having problem in getting the Camera ID to activate the torch mode

--- app/src/main/java/com/example/android/flashligh_banban/MainActivity.java
+++ app/src/main/java/com/example/android/flashligh_banban/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.flashligh_banban;
 
 import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,20 +15,45 @@ import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
-    CameraManager camera = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-
     public void state(View view){
         Switch getState = (Switch) findViewById(R.id.switch1);
         Boolean isChecked = getState.isChecked();
         Log.i("State","Is " + isChecked);
-
+        CameraManager camera = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        on();
+//        onOff(isChecked, camera);
     }
 
+    public void on(){
+        CameraManager camera = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            try {
+                for (String idCamera : camera.getCameraIdList()){
+//                    camera.setTorchMode(idCamera,true);
+                    continue;
+                }
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+    }
+
+//    public void onOff(Boolean isChecked, CameraManager camera){
+//        //getting the camera id
+//        if (isChecked){
+//            try {
+//                for (String idCamera : camera.getCameraIdList()){
+//                    camera.setTorchMode(idCamera,true);
+//                    continue;
+//                }
+//            } catch (CameraAccessException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//    }
 
 }
